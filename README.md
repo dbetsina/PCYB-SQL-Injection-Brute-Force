54b19b62e1fab2be8deb44d53408fbe5f5271719
Dana Betsina <bettkina@gmail.com>
2024-11-02 19:09:44 +0100

    database connected to login form + authorized n unauthorized pages created

---
+++ README.md
@@ -0,0 +1,36 @@
+# Cyber-Pro App
+
+This project is a demonstration application for testing hacking techniques such as SQL Injection and Brute Force attacks. The application provides a set of web pages to explore and understand these techniques in a controlled environment.
+
+## Prerequisites
+
+- Python 3.8+
+- Virtualenv
+
+## Installation
+
+
+1. Set up the virtual environment and install dependencies:
+
+    ```bash
+    python3 -m venv venv
+    source venv/bin/activate
+    pip install -r requirements.txt
+    ```
+
+2. Initialize the database:
+
+    ```bash
+    python database_creation.py
+    ```
+
+## Running the Application
+
+1. Start the application:
+
+    ```bash
+    python app.py
+    ```
+
+2. Open your browser and navigate to [http://localhost:5000] to access the application.
+
--- cyber_app/app.py
+++ cyber_app/app.py
@@ -1,10 +1,47 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, redirect, url_for, flash, session
+import sqlite3
+import hashlib
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
+def validate_user(email, password):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    hashed_password = hash_password(password)
+    cursor.execute('SELECT * FROM users WHERE email = ? AND password = ?', (email, hashed_password))
+    user = cursor.fetchone()
+    conn.close()
+    return user
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/user')
+def user():
+    username = session.get('username', 'User')
+    return render_template('user.html', username=username)
+
+@app.route('/hacker')
+def hacker():
+    return render_template('hacker.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form['email']
+    password = request.form['password']
+    user = validate_user(email, password)
+    if user:
+        session['username'] = user[1]  
+        flash(f"Welcome, {user[1]}!", "success")
+        return redirect(url_for('user'))
+    else:
+        flash("Invalid email or password", "danger")
+        return redirect(url_for('hacker'))
 
 if __name__ == '__main__':
     app.run(debug=True)
